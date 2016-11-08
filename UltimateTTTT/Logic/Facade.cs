14b697eb8bf83f749ed352a7feb49834ab871879
filip jägerud <filni@student.liu.se>
2016-11-08 15:51:00 +0100

    Add project files.

---
+++ UltimateTTTT/Logic/Facade.cs
@@ -0,0 +1,12 @@
+﻿vusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateTTTT.Logic
+{
+    class Facade
+    {
+    }
+}

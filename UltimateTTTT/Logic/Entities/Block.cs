962a37051d4f159615b4f80e1c473f653eb2ec21
filip jägerud <filni@student.liu.se>
2016-11-08 16:40:41 +0100

    added owner in block

--- UltimateTTTT/Logic/Entities/Block.cs
+++ UltimateTTTT/Logic/Entities/Block.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace UltimateTTTT.Logic.Entities
 {
+    
     class Block
     {
+        private int owner = 0;
+
+        public void setOwner(int owner)
+        {
+            this.owner = owner;
+        }
+        public int getOwner()
+        {
+            return owner;
+        }
     }
 }

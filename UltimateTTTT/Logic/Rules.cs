bbfa6f60c95e56f26fa1ac0417534f240ce20a4b
filip jägerud <filni@student.liu.se>
2016-11-10 17:43:19 +0100

    started rules

---
+++ UltimateTTTT/Logic/Rules.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateTTTT.Entities;
+
+namespace UltimateTTTT.Logic
+{
+    class Rules
+    {
+        public void LockedSquare(Position position)
+        {
+            if (position.GetBlockPosition() == 0)
+            {
+                //light up square 0
+                
+            }
+            if (position.GetBlockPosition() == 1)
+            {
+                //light up square 0
+
+            }
+            if (position.GetBlockPosition() == 2)
+            {
+                //light up square 0
+
+            }
+            if (position.GetBlockPosition() == 3)
+            {
+                //light up square 0
+
+            }
+            if (position.GetBlockPosition() == 4)
+            {
+                //light up square 0
+
+            }
+            if (position.GetBlockPosition() == 5)
+            {
+                //light up square 0
+
+            }
+            if (position.GetBlockPosition() == 6)
+            {
+                //light up square 0
+
+            }
+            if (position.GetBlockPosition() == 7)
+            {
+                //light up square 0
+
+            }
+            if (position.GetBlockPosition() == 8)
+            {
+                //light up square 0
+
+            }
+        }
+    }
+}

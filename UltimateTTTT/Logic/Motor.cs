6d0990fe7cd8c817604e98fb1be204d60948cb5a
filip jägerud <filni@student.liu.se>
2016-11-22 12:09:07 +0100

    added class Motor

---
+++ UltimateTTTT/Logic/Motor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateTTTT.Entities;
+using UltimateTTTT.Logic.Entities;
+
+namespace UltimateTTTT.Logic
+{
+    class Motor
+    {
+        //not sure about variables
+        private Board board;
+        private Rules rules;
+        private Position position = new Position();
+
+        public Motor(Board board)
+        {
+            this.board= board;
+        }
+
+        public void Move(Position position, Player player)
+        {
+            if (board.GetSquare()[position.GetSquarePosition()].Blocks[position.GetBlockPosition()].getOwner() == 0)
+                //free
+            {
+                board.GetSquare()[position.GetSquarePosition()].Blocks[position.GetBlockPosition()].setOwner(player);
+                    //player as owner
+            }
+            else
+            {
+                //block 
+            }
+            //check if winner
+            rules.Winner(board.GetSquare()[position.GetSquarePosition()], board.GetPlayers());
+
+        }
+
+    }
+}

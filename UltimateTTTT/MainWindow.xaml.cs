57d32351593926a802bc992e7554d4c19fca3645
filip jägerud <filni@student.liu.se>
2016-11-23 14:48:22 +0100

    changed declaration of blocks

--- UltimateTTTT/MainWindow.xaml.cs
+++ UltimateTTTT/MainWindow.xaml.cs
@@ -27,34 +27,30 @@ namespace UltimateTTTT
         {
             Rules rules = new Rules();
             Position position = new Position();
-            Blocks[] blocks = new Blocks[9];
+            Blocks[] blocks0 = new Blocks[9];
+            Blocks[] blocks1 = new Blocks[9];
+            Blocks[] blocks2 = new Blocks[9];
+            Blocks[] blocks3 = new Blocks[9];
+            Blocks[] blocks4 = new Blocks[9];
+            Blocks[] blocks5 = new Blocks[9];
+            Blocks[] blocks6 = new Blocks[9];
+            Blocks[] blocks7 = new Blocks[9];
+            Blocks[] blocks8 = new Blocks[9];
             Square[] squares = new Square[9];
+            squares[0] = new Square(blocks0);
+            squares[1] = new Square(blocks1);
+            squares[2] = new Square(blocks2);
+            squares[3] = new Square(blocks3);
+            squares[4] = new Square(blocks4);
+            squares[5] = new Square(blocks5);
+            squares[6] = new Square(blocks6);
+            squares[7] = new Square(blocks7);
+            squares[8] = new Square(blocks8);
+
             Player player1 = new Player(1);
             Player player2 = new Player(2);
             Player[] players = new Player[2];
-            int counter = 0;
-
-            for (int i = 0; i < squares.Length; i++)
-            {
-                for (int j = 0; j < blocks.Length; j++)
-                {
-                    blocks[j] = new Blocks();
-                    blocks[j].setCounter(counter);
-                    counter++;
-
-                }
-                squares[i] = new Square(blocks);
-            }
-            for (int i = 0; i < squares.Length; i++)
-            {
-                for (int j = 0; j < blocks.Length; j++)
-                {
-                    //counter++;
-                    System.Console.WriteLine("block: " + counter + " " + blocks[j].getCounter());
-                }
-
-            }
-
+            
             players[0] = player1;
             players[1] = player2;
             Board board = new Board(squares, players);     //insert everything in constructor
